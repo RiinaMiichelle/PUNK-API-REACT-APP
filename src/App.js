315b229953f3786d6a661efe9f213ef796d5bd35
RiinaMiichelle <sarina.m.colosimo@gmail.com>
2020-03-14 18:35:18 -0500

    working on punk api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      beers: []
+    }
+  }
+
+  componentDidMount= () => {
+    fetch('https://api.punkapi.com/v2/beers')
+          .then((resp) => {
+            return resp.json();
+          })
+          .then((beers) => {
+            this.setState({
+              beers: beers 
+            });
+          });
+  }
+
+  // displayBeerInfo = () => {
+  //   var beerList = this.state.beers
+  //   console.log(beerList);
+  // }
+
+
+  render () {
+    // console.log('state: ' + JSON.stringify(this.state));
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+  
 }
+
+
 
 export default App;
